82a0d37aa4658bbf8e89c57233b8931ff0aa7eb9
cod3prod <cod3prod@gmail.com>
2024-08-15 20:39:12 +0900

    scroll-master

---
+++ scroll-master/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Hedaer = () => {
+    return (
+        <div>
+            {/* 내용 */}
+        </div>
+    );
+};
+
+export default Header;
---
+++ scroll-master/src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.css'
+
+const Main = () => {
+    return (
+        <div>
+            {/* 내용 */}
+        </div>
+    );
+};
+
+export default Main;
