76e49ca52b76ee38177c7f1062d2147104ccfb1e
cod3prod <cod3prod@gmail.com>
2024-08-30 20:40:55 +0900

    scroll-master

--- scroll-master/src/components/main/Parallax.jsx
+++ scroll-master/src/components/main/Parallax.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import './Parallax.css';
 
 function SeasonText() {
     return (
         <div className='season-text'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi earum sapiente tempore ex in quam consequuntur obcaecati nemo iusto eos quas labore praesentium, maxime harum et dolores dolore sequi maiores.
-            Quas autem culpa incidunt sequi ratione dolorem laudantium nihil voluptate dolores. Libero omnis aspernatur minima soluta. Voluptate accusamus, repellat nihil excepturi id odio reiciendis maxime assumenda, quod atque esse ad!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit...
         </div>
     );
 }
@@ -12,15 +12,13 @@ function SeasonText() {
 export default function SeasonImages() {
     const seasons = ['spring', 'summer', 'fall', 'winter'];
     const seasonImages = seasons.map((el, idx) => {
-        const backgroundImage = `/images/${el}.jpg`;
+        const divStyle = {
+            backgroundImage: `url(${process.env.PUBLIC_URL}/images/${el}.jpg)`,
+        };
 
         return (
             <div key={idx} className='season-container'>
-                <div 
-                    className='season-image' 
-                    style={{ backgroundImage: 'url(backgroundImage)' }}
-                >
-                </div>
+                <div className='season-image' style={divStyle}></div>
                 <SeasonText />
             </div>
         );
